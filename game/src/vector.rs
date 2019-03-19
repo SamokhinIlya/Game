b6cc3ce635a54ea490234c1f4892212dc37c8345
Samokhin Ilya <34774108+SamokhinIlya@users.noreply.github.com>
2019-03-19 09:03:59 +0300

    Revert "Integer vector type (#5)" (#6)
    This reverts commit 3c9d068ad8f8705af83a1a957c5433afcc644619.

--- game/src/vector.rs
+++ game/src/vector.rs
@@ -1,28 +1,14 @@
-use core::fmt::Debug;
 use core::ops::{
     Add, AddAssign,
-    Sub, SubAssign,
-    Mul, MulAssign,
+    Sub,
     Neg,
+    Mul,
 };
 
-pub trait Num32:
-    Sized + Copy + Clone
-    + Send + Sync
-    + Debug
-    + Add<Output=Self> + AddAssign
-    + Sub<Output=Self> + SubAssign
-    + Mul<Output=Self> + MulAssign
-    + Neg<Output=Self>
-{}
-
-impl Num32 for i32 {}
-impl Num32 for f32 {}
-
-#[derive(Copy, Clone, Debug)]
-pub struct V2<T: Num32> {
-    pub x: T,
-    pub y: T,
+#[derive(Copy, Clone, Default, Debug)]
+pub struct V2 {
+    pub x: f32,
+    pub y: f32,
 }
 
 macro_rules! v2 {
@@ -31,77 +17,61 @@ macro_rules! v2 {
     };
 }
 
-impl<T: Num32> V2<T> {
-    pub fn new(x: T, y: T) -> Self {
-        V2 { x, y }
-    }
+impl V2 {
+    pub const ZERO: Self = v2!(0.0, 0.0);
 }
 
-impl<T: Num32> Add for V2<T> {
-    type Output = Self;
-    fn add(self, rhs: Self) -> Self::Output {
-        V2::new(self.x + rhs.x, self.y + rhs.y)
+impl Add for V2 {
+    type Output = V2;
+    fn add(self, rhs: V2) -> V2 {
+        v2!(self.x + rhs.x, self.y + rhs.y)
     }
 }
-
-impl<T: Num32> AddAssign for V2<T> {
-    fn add_assign(&mut self, rhs: Self) {
+impl AddAssign for V2 {
+    fn add_assign(&mut self, rhs: V2) {
         *self = *self + rhs;
     }
 }
 
-impl<T: Num32> Sub for V2<T> {
-    type Output = Self;
-    fn sub(self, rhs: Self) -> Self::Output {
-        V2::new(self.x - rhs.x, self.y - rhs.y)
+impl Sub for V2 {
+    type Output = V2;
+    fn sub(self, rhs: V2) -> V2 {
+        v2!(self.x - rhs.x, self.y - rhs.y)
     }
 }
 
-impl<T: Num32> SubAssign for V2<T> {
-    fn sub_assign(&mut self, rhs: Self) {
-        *self = *self - rhs;
+impl Neg for V2 {
+    type Output = V2;
+    fn neg(self) -> Self {
+        v2!(-self.x, -self.y)
     }
 }
 
-impl<T: Num32> Mul<T> for V2<T> {
-    type Output = Self;
-    fn mul(self, rhs: T) -> Self::Output {
-        V2::new(self.x * rhs, self.y * rhs)
+impl Mul<f32> for V2 {
+    type Output = V2;
+    fn mul(self, scalar: f32) -> V2 {
+        v2!(self.x * scalar, self.y * scalar)
     }
 }
 
-impl Mul<V2<i32>> for i32 {
-    type Output = V2<i32>;
-    fn mul(self, rhs: Self::Output) -> Self::Output {
-        V2::new(self * rhs.x, self * rhs.y)
+impl Mul<V2> for f32 {
+    type Output = V2;
+    fn mul(self, v: V2) -> V2 {
+        v * self
     }
 }
 
-impl Mul<V2<f32>> for f32 {
-    type Output = V2<f32>;
-    fn mul(self, rhs: Self::Output) -> Self::Output {
-        V2::new(self * rhs.x, self * rhs.y)
-    }
-}
-
-impl<T: Num32> Neg for V2<T> {
-    type Output = Self;
-    fn neg(self) -> Self::Output {
-        V2::new(-self.x, -self.y)
-    }
-}
-
-pub fn dot<T: Num32>(lhs: V2<T>, rhs: V2<T>) -> T {
+pub fn dot(lhs: V2, rhs: V2) -> f32 {
     lhs.x * rhs.x + lhs.y * rhs.y
 }
 
-pub fn distance_sq<T: Num32>(lhs: V2<T>, rhs: V2<T>) -> T {
+pub fn distance_sq(lhs: V2, rhs: V2) -> f32 {
     let distance_vector = rhs - lhs;
     dot(distance_vector, distance_vector)
 }
 
 use std::fmt;
-impl fmt::Display for V2<f32> {
+impl fmt::Display for V2 {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         write!(f, "V2({:.2}, {:.2})", self.x, self.y)
     }
